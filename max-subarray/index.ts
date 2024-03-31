b053ee3852142e599eac068821a19a746beb3cd6
Sufiane <su.souissi@gmail.com>
2024-03-31 15:37:56 +0200

    wip: max-subarray

--- max-subarray/index.ts
+++ max-subarray/index.ts
@@ -1,8 +1,17 @@
+const lint = 'https://leetcode.com/problems/length-of-longest-subarray-with-at-most-k-frequency/description/'
+
 //const array = [1, 2, 3, 1, 2, 3, 1, 2]
 //const maxOccurrence = 2
 
-//const array = [1]
-const array = [2, 2, 3]
+// const array = [1]
+// const array = [2, 2, 3]
+// const maxOccurrence = 1
+
+// const array = [1, 1, 1, 3]
+// const maxOccurrence = 2
+
+// todo problem get for instead of 2
+const array = [1, 2, 1, 2, 1, 2, 1, 2]
 const maxOccurrence = 1
 
 export function maxSubarrayLength(nums: number[], k: number): number {
@@ -15,31 +24,36 @@ export function maxSubarrayLength(nums: number[], k: number): number {
     let biggestLength = 0
     let tmpMap = new Map<number, number>()
 
-    while (left < nums.length) {
-        let tmpLength = 0
+    while (right < nums.length) {
+        let occurrence = tmpMap.get(nums[right]) ?? 0
 
-        while (right < nums.length) {
-            const occurrence = tmpMap.get(nums[right])
-
-            if (!occurrence) {
-                tmpMap.set(nums[right], 1)
-                tmpLength++
-            } else if (occurrence + 1 <= k) {
-                tmpMap.set(nums[right], occurrence + 1)
-                tmpLength++
-            } else {
-                tmpMap.clear()
-                break;
-            }
-
-            right++
+        if (occurrence === 0) {
+            tmpMap.set(nums[right], occurrence)
         }
 
-        left++
-        right = left
+        console.log('toto', {
+            left,
+            right,
+            numsleft: nums[left],
+            numsright: nums[right],
+            occurrence,
+        })
 
-        biggestLength = tmpLength > biggestLength ? tmpLength : biggestLength
+        tmpMap.set(nums[right], occurrence + 1)
+
+        while (occurrence + 1 > k && left < right) {
+            console.log({ occurrence, left, right, value: nums[right] })
+            occurrence--
+            tmpMap.set(nums[left], occurrence)
+
+            left++
+        }
+
+        biggestLength = Math.max(biggestLength, right - left + 1)
+        right++
     }
+
+    console.log('map final', { tmpMap })
 
     return biggestLength
 }
