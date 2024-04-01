33b3c32fa85bd5088fe996a775799546b82f1388
Sufiane <su.souissi@gmail.com>
2024-04-01 16:31:34 +0200

    feat(two-sum): easy working solution

---
+++ two-sum/indext.ts
@@ -0,0 +1,11 @@
+import { describe, it } from 'node:test';
+import { twoSum } from './index';
+import assert from 'node:assert';
+
+describe('two sum', () => {
+    describe('nums = [2,7,11,15], target = 9', () => {
+        it('should return [0, 1]', () => {
+            assert.deepEqual(twoSum([2, 7, 11, 15], 9), [0, 1])
+        });
+    })
+})
