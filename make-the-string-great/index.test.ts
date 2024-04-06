aa3210b8dfb409e7b8fd14b7528b4bd4a45f8f81
Sufiane <su.souissi@gmail.com>
2024-04-06 13:30:55 +0200

    feat(make-the-string-great): easy answer

---
+++ make-the-string-great/index.test.ts
@@ -0,0 +1,30 @@
+import assert from 'node:assert';
+import { describe, it } from 'node:test';
+
+import { makeGood } from './index';
+
+describe('make the string great', () => {
+    describe('s = "leEeetcode"', () => {
+        it('should return "leetcode"', () => {
+            assert.strictEqual(makeGood("leEeetcode"), "leetcode")
+        })
+    })
+
+    describe('s = "abBAcC"', () => {
+        it('should return ""', () => {
+            assert.strictEqual(makeGood("abBAcC"), "")
+        })
+    })
+
+    describe('s = "s"', () => {
+        it('should return "s"', () => {
+            assert.strictEqual(makeGood("s"), "s")
+        })
+    })
+
+    describe('s = "mC"', () => {
+        it('should return "mC"', () => {
+            assert.strictEqual(makeGood("mC"), "mC")
+        })
+    })
+})
---
+++ make-the-string-great/index.ts
@@ -0,0 +1,27 @@
+const isUpperCased = str => /[A-Z]/.test(str);
+
+export function makeGood(s: string): string {
+    if (s.length <= 1) {
+        return s
+    }
+
+    let i = 0
+    const letters = s.split('')
+
+    while (i < letters.length - 1) {
+        if (
+            letters[i].toLowerCase() === letters[i + 1].toLowerCase()
+            && (
+                (isUpperCased(letters[i]) && !isUpperCased(letters[i + 1]))
+                || (!isUpperCased(letters[i]) && isUpperCased(letters[i + 1]))
+            )
+        ) {
+            letters.splice(i, 2)
+            i += i > 0 ? -1 : 0
+        } else {
+            i++
+        }
+    }
+
+    return letters.join('')
+};
