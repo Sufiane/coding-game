0b46c7098fda462383fff957852a63e8bf7f2b5b
Sufiane <su.souissi@gmail.com>
2024-04-07 20:33:18 +0200

    feat(valid-parenthesis-string): answer

---
+++ valid-parenthesis-string/index.test.ts
@@ -0,0 +1,69 @@
+import assert from 'node:assert';
+import { describe, it } from 'node:test';
+
+import { checkValidString } from './index';
+
+describe('checkValidString', () => {
+    describe('s = "()"', () => {
+        it('should return true', () => {
+            assert.strictEqual(checkValidString('()'), true)
+        })
+    })
+
+    describe('s = "(*)"', () => {
+        it('should return true', () => {
+            assert.strictEqual(checkValidString('(*)'), true)
+        })
+    })
+
+    describe('s = "(*))"', () => {
+        it('should return true', () => {
+            assert.strictEqual(checkValidString('(*))'), true)
+        })
+    })
+
+    describe(
+        's = "((((()(()()()*()(((((*)()*(**(())))))(())()())(((())())())))))))(((((())*)))()))(()((*()*(*)))(*)()"',
+        () => {
+            it('should return true', () => {
+                assert.strictEqual(
+                    checkValidString(
+                        '((((()(()()()*()(((((*)()*(**(())))))(())()())(((())())())))))))(((((())*)))()))(()((*()*(*)))(*)()'),
+                    true,
+                )
+            })
+        },
+    )
+
+    describe(
+        's = "(((((*(()((((*((**(((()()*)()()()*((((**)())*)*)))))))(())(()))())((*()()(((()((()*(())*(()**)()(())"',
+        () => {
+            it('should return false', () => {
+                assert.strictEqual(
+                    checkValidString(
+                        '(((((*(()((((*((**(((()()*)()()()*((((**)())*)*)))))))(())(()))())((*()()(((()((()*(())*(()**)()(())'),
+                    false,
+                )
+            })
+        },
+    )
+
+    describe(
+        's = "(((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((("',
+        () => {
+            it('should return false', () => {
+                assert.strictEqual(
+                    checkValidString(
+                        '(((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((('),
+                    false,
+                )
+            })
+        },
+    )
+
+    describe('s = "*)*())"', () => {
+        it('should return true', () => {
+            assert.strictEqual(checkValidString('*)*())'), true)
+        })
+    })
+})
---
+++ valid-parenthesis-string/index.ts
@@ -0,0 +1,47 @@
+export function checkValidString(s: string): boolean {
+    const characters = s.split('');
+
+    let openStack: number[] = []
+    let wildCardStack: number[] = []
+
+    if (s.length < 2 && s[0] !== '*') {
+        console.log('early exit')
+        return false
+    }
+
+    for (let i = 0; i < characters.length; i++) {
+        if (characters[i] === '(') {
+            openStack.push(i)
+
+        } else if (characters[i] === ')') {
+            if (openStack.length > 0) {
+                openStack.pop()
+            } else {
+                const lastWildCard = wildCardStack.at(-1)
+
+                if (lastWildCard === undefined) {
+                    return false
+                }
+
+                wildCardStack.pop()
+            }
+        } else if (characters[i] === '*') {
+            wildCardStack.push(i)
+        }
+    }
+
+    while (openStack.length > 0) {
+        if (wildCardStack.length === 0) {
+            return false
+        }
+
+        const lastOpen = openStack.pop()
+        const lastWildCard = wildCardStack.pop()
+
+        if (lastOpen > lastWildCard) {
+            return false
+        }
+    }
+
+    return true
+};
