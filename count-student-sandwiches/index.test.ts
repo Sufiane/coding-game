948c6a6aa55ef66661d04a8fc314dc610a4c1bdd
Sufiane <su.souissi@gmail.com>
2024-04-08 18:19:42 +0200

    feat(count-student-sandwiches): easy and fast answer

---
+++ count-student-sandwiches/index.test.ts
@@ -0,0 +1,17 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
+import { countStudents } from './index';
+
+describe('CountStudents', () => {
+    describe('students = [1,1,0,0], sandwiches = [0,1,0,1]', () => {
+        it('should return 0', () => {
+            assert.strictEqual(countStudents([1, 1, 0, 0], [0, 1, 0, 1]), 0)
+        })
+    })
+
+    describe('students = [1,1,1,0,0,1], sandwiches = [1,0,0,0,1,1]', () => {
+        it('should return 3', () => {
+            assert.strictEqual(countStudents([1, 1, 1, 0, 0, 1], [1, 0, 0, 0, 1, 1]), 3)
+        })
+    })
+})
---
+++ count-student-sandwiches/index.ts
@@ -0,0 +1,14 @@
+export function countStudents(students: (0 | 1)[], sandwiches: (0 | 1)[]): number {
+
+    while (students.some(student => student === sandwiches[0])) {
+        if (students[0] === sandwiches[0]) {
+            students.shift()
+            sandwiches.shift()
+
+        } else {
+            students.push(students.shift())
+        }
+    }
+
+    return students.length
+}
