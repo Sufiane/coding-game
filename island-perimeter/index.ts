8f3876bdde9604fa6efd0f0f57cda095ee67fde7
Sufiane <su.souissi@gmail.com>
2024-04-18 14:22:42 +0200

    fix(islandPerimeter): improvment

--- island-perimeter/index.ts
+++ island-perimeter/index.ts
@@ -1,25 +1,18 @@
 export function islandPerimeter(grid: number[][]): number {
     let perimeter = 0
-    const coordToCheck: number[][] = []
 
     // we get all the land coordinates
-    for (let line = 0; line < grid.length; line++) {
-        for (let col = 0; col < grid[line].length; col++) {
-            if (grid[line][col] === 1) {
-                coordToCheck.push([line, col])
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[row].length; col++) {
+            if (grid[row][col] === 1) {
+                perimeter +=
+                    4 -
+                    (grid[row - 1]?.[col] ?? 0) -
+                    (grid[row + 1]?.[col] ?? 0) -
+                    (grid[row]?.[col - 1] ?? 0) -
+                    (grid[row]?.[col + 1] ?? 0)
             }
         }
-    }
-
-    for (let i = 0; i < coordToCheck.length; i++) {
-        const [row, col] = coordToCheck[i]
-
-        perimeter +=
-            4 -
-            (grid[row - 1]?.[col] ?? 0) -
-            (grid[row + 1]?.[col] ?? 0) -
-            (grid[row]?.[col - 1] ?? 0) -
-            (grid[row]?.[col + 1] ?? 0)
     }
 
     return perimeter
