1bba068df60da3b5b089f1ecc57ffd41a73d3058
Sufiane <su.souissi@gmail.com>
2024-04-24 16:56:43 +0200

    fix(climb-stairs): faster answer

--- climb-stairs/index.ts
+++ climb-stairs/index.ts
@@ -1,4 +1,5 @@
-export function climbStairs(n: number): number {
+// this take more time than the one below
+export function climbStairsRecursive(n: number): number {
     if (n === 1) {
         return 1
     }
@@ -7,5 +8,34 @@ export function climbStairs(n: number): number {
         return 2
     }
 
-    return climbStairs(n - 1) + climbStairs(n - 2)
+    return climbStairsRecursive(n - 1) + climbStairsRecursive(n - 2)
+}
+
+export function climbStairs(n: number): number {
+    if (n <= 0) {
+        return 0
+    }
+
+    if (n === 1) {
+        return 1
+    }
+
+    if (n === 2) {
+        return 2
+    }
+
+    let lastStep = 2
+    let lastStep2 = 1
+    let currentNbWays = 0
+
+    for (let i = 3; i <= n; i++) {
+        let currentStep = lastStep + lastStep2
+
+        currentNbWays = currentStep
+
+        lastStep2 = lastStep
+        lastStep = currentStep
+    }
+
+    return currentNbWays
 }
